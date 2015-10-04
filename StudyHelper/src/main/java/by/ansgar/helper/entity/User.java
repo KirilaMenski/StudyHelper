ce4dbc0e40263a88e27bb43d057c9bfcd21cd49d
Mensky <bv_1514@mail.ru>
2015-10-04 19:04:02 +0300

    Created entity

---
+++ StudyHelper/src/main/java/by/ansgar/helper/entity/User.java
@@ -0,0 +1,73 @@
+package by.ansgar.helper.entity;
+
+public class User {
+
+	private long id;
+	private String name;
+	private String surname;
+	private String password;
+	private String email;
+	private int phone;
+
+	public User() {
+
+	}
+
+	public User(long id, String name, String surname, String password, String email, int phone) {
+		this.id = id;
+		this.name = name;
+		this.surname = surname;
+		this.password = password;
+		this.email = email;
+		this.phone = phone;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getPhone() {
+		return phone;
+	}
+
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
+}
