5d2a1c0da08898546814445117b450d97c6b62d1
Mensky <bv_1514@mail.ru>
2015-10-06 20:15:05 +0300

    Added deleting process

--- StudyHelper/src/main/java/by/ansgar/helper/controller/AddPageController.java
+++ StudyHelper/src/main/java/by/ansgar/helper/controller/AddPageController.java
@@ -2,10 +2,13 @@ package by.ansgar.helper.controller;
 
 import java.sql.SQLException;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,7 +21,7 @@ public class AddPageController {
 
 	@Autowired
 	private UserService userService;
-	
+
 	@RequestMapping(value = "/add_page")
 	public ModelAndView doAddPage() {
 		ModelAndView mav = new ModelAndView();
@@ -37,9 +40,34 @@ public class AddPageController {
 		}
 		return "main";
 	}
+
+	@RequestMapping(value = "/add_usersFromFile", method = { RequestMethod.GET, RequestMethod.POST })
+	public String addUserdFromFile( HttpServletRequest request) {
+
+		String name = request.getParameter("file_name");
+		System.out.println(name);
+		try {
+			userService.addUserFromFile(name);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return "forward:/show_all";
+	}
 	
+	@RequestMapping(value="/delete_user_{id}", method = { RequestMethod.GET, RequestMethod.POST })
+	public String deleteUser(@ModelAttribute User user, @PathVariable long id, BindingResult result){
+		try {
+			user.setId(id);
+			userService.deleteUser(user);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return "forward:/show_all";
+	}
+
 	@ModelAttribute("users")
-	public User user(){
+	public User user() {
 		return new User();
 	}
 
