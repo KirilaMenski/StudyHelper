860c97c2453dc423745b636e91ddc5364a9c65ac
Mensky <bv_1514@mail.ru>
2015-10-07 20:14:52 +0300

    Controller for update students

--- StudyHelper/src/main/java/by/ansgar/helper/controller/StudentsListController.java
+++ StudyHelper/src/main/java/by/ansgar/helper/controller/StudentsListController.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import by.ansgar.helper.entity.Students;
@@ -17,6 +21,7 @@ public class StudentsListController {
 
 	@Autowired
 	private StudentService studentsService;
+	private static long idStudent;
 
 	@RequestMapping(value = "/show_all")
 	public ModelAndView showAll() {
@@ -32,9 +37,48 @@ public class StudentsListController {
 
 		return mav;
 	}
-	
+
+	@RequestMapping(value = "/delete_student_{id}", method = { RequestMethod.GET, RequestMethod.POST })
+	public String deleteStudent(@ModelAttribute Students students, @PathVariable long id, BindingResult result) {
+		try {
+			students.setId(id);
+			studentsService.deleteStudent(students);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return "forward:/show_all";
+	}
+
+	@RequestMapping(value = "/student_profile_{id}", method = { RequestMethod.POST, RequestMethod.GET })
+	public ModelAndView showProfile(@PathVariable long id, @ModelAttribute Students student, BindingResult result) {
+		ModelAndView mav = new ModelAndView();
+		idStudent = id;
+		try {
+			//Students student = new Students();
+			student = studentsService.getStudentById(id);
+			mav.addObject("student", student);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		mav.setViewName("student_profile");
+		return mav;
+	}
+
+	@RequestMapping(value = "/edit_student", method = { RequestMethod.POST, RequestMethod.GET })
+	public String editStudent(@ModelAttribute Students student, BindingResult result) {
+		try {
+			student.setId(idStudent);
+			System.out.println(idStudent);
+			studentsService.editStudent(student);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return "forward:/show_all";
+		
+	}
+
 	@ModelAttribute("students")
-	public Students students(){
+	public Students students() {
 		return new Students();
 	}
 
