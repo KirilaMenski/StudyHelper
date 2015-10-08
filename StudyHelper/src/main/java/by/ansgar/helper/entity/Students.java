40e29688273c5c816ddcb59c6381f712830606ff
Mensky <bv_1514@mail.ru>
2015-10-08 12:27:22 +0300

    Added new column

--- StudyHelper/src/main/java/by/ansgar/helper/entity/Students.java
+++ StudyHelper/src/main/java/by/ansgar/helper/entity/Students.java
@@ -10,33 +10,36 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "students")
 public class Students {
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	@Column(name="ID")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ID")
 	private long id;
-	@Column(name="NAME")
+	@Column(name = "NAME")
 	private String name;
-	@Column(name="SURNAME")
+	@Column(name = "SURNAME")
 	private String surname;
-	@Column(name="PASSWORD")
+	@Column(name = "PASSWORD")
 	private String password;
-	@Column(name="EMAIL")
+	@Column(name = "EMAIL")
 	private String email;
-	@Column(name="phone")
+	@Column(name = "phone")
 	private String phone;
+	@Column(name = "class")
+	private String group;
 
 	public Students() {
 
 	}
 
-	public Students(long id, String name, String surname, String password, String email, String phone) {
+	public Students(long id, String name, String surname, String password, String email, String phone, String group) {
 		this.id = id;
 		this.name = name;
 		this.surname = surname;
 		this.password = password;
 		this.email = email;
 		this.phone = phone;
+		this.group = group;
 	}
 
 	public long getId() {
@@ -85,6 +88,14 @@ public class Students {
 
 	public void setPhone(String phone) {
 		this.phone = phone;
+	}
+
+	public String getGroup() {
+		return group;
+	}
+
+	public void setGroup(String group) {
+		this.group = group;
 	}
 
 }
