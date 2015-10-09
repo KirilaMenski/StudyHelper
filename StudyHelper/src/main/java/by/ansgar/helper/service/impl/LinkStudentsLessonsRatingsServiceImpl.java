979e549ea1b0eb3c0c6b2d36317418e3be5f7e1a
Mensky <bv_1514@mail.ru>
2015-10-09 17:06:14 +0300

    Modified

--- StudyHelper/src/main/java/by/ansgar/helper/service/impl/LinkStudentsLessonsRatingsServiceImpl.java
+++ StudyHelper/src/main/java/by/ansgar/helper/service/impl/LinkStudentsLessonsRatingsServiceImpl.java
@@ -1,5 +1,44 @@
 package by.ansgar.helper.service.impl;
 
-public class LinkStudentsLessonsRatingsServiceImpl {
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import by.ansgar.helper.dao.LinkStudentsLessonsRatingsDAO;
+import by.ansgar.helper.entity.LinkStudentsLessonsRating;
+import by.ansgar.helper.service.LinkStudentsLessonsRatingsService;
+
+@Service
+public class LinkStudentsLessonsRatingsServiceImpl implements LinkStudentsLessonsRatingsService{
+	
+	@Autowired
+	private LinkStudentsLessonsRatingsDAO linkSLRDAO;
+
+	@Transactional
+	public void addLink(LinkStudentsLessonsRating linkSLR) throws SQLException {
+		linkSLRDAO.addLink(linkSLR);
+		
+	}
+
+	@Transactional
+	public List<LinkStudentsLessonsRating> getlessons(long id)
+			throws SQLException {
+		List<LinkStudentsLessonsRating> lessonsById = new ArrayList<LinkStudentsLessonsRating>();
+		lessonsById = linkSLRDAO.getlessons(id);
+		return lessonsById;
+	}
+
+	@Transactional
+	public List<LinkStudentsLessonsRating> getRating(long id)
+			throws SQLException {
+		List<LinkStudentsLessonsRating> ratingById = new ArrayList<LinkStudentsLessonsRating>();
+		ratingById = linkSLRDAO.getRating(id);
+		return ratingById;
+	}
 
 }
--- StudyHelper/src/main/java/by/ansgar/helper/service/impl/RatingsServiceImpl.java
+++ StudyHelper/src/main/java/by/ansgar/helper/service/impl/RatingsServiceImpl.java
@@ -1,5 +1,32 @@
 package by.ansgar.helper.service.impl;
 
-public class RatingsServiceImpl {
+import java.sql.SQLException;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import by.ansgar.helper.dao.RatingsDAO;
+import by.ansgar.helper.entity.Ratings;
+import by.ansgar.helper.service.RatingsService;
+
+@Service
+public class RatingsServiceImpl implements RatingsService{
+	
+	@Autowired
+	private RatingsDAO ratingsDAO;
+
+	@Transactional
+	public void addRating(Ratings ratings) throws SQLException {
+		ratingsDAO.addRating(ratings);
+		
+	}
+
+	@Transactional
+	public void editRating(Ratings ratings) throws SQLException {
+		ratingsDAO.editRating(ratings);
+		
+	}
 
 }
