8e6b70217e48e43bcdd9da6347b46ad13e66b046
Mensky <bv_1514@mail.ru>
2015-10-15 21:19:50 +0300

    DAO method for link students with ratings

--- StudyHelper/src/main/java/by/ansgar/helper/dao/LinkStudentsLessonsRatingsDAO.java
+++ StudyHelper/src/main/java/by/ansgar/helper/dao/LinkStudentsLessonsRatingsDAO.java
@@ -6,14 +6,19 @@ import java.util.List;
 import by.ansgar.helper.entity.LinkStudentsLessonsRating;
 
 public interface LinkStudentsLessonsRatingsDAO {
-	
+
 	public void addLink(LinkStudentsLessonsRating linkSLR) throws SQLException;
-	
-	public List<LinkStudentsLessonsRating> getStudLessons (long id) throws SQLException;
-	
-	public List<LinkStudentsLessonsRating> getLessRatings (long id) throws SQLException;
-	
-	public List<LinkStudentsLessonsRating> getStudRatings(long id) throws SQLException;
-	
+
+	public List<LinkStudentsLessonsRating> getStudLessons(long id)
+			throws SQLException;
+
+	public List<LinkStudentsLessonsRating> getLessRatings(long id)
+			throws SQLException;
+
+	public List<LinkStudentsLessonsRating> getStudRatings(long id)
+			throws SQLException;
+
+	public List<LinkStudentsLessonsRating> getRatingsByStudAndLesson(
+			long lessonId, long studentId) throws SQLException;
 
 }
