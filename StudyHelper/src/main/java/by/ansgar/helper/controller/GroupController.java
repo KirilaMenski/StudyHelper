7e88f114655edfb692f65299677a20a7d33ed56f
Mensky <bv_1514@mail.ru>
2015-10-16 19:29:07 +0300

    Added function to display students ratings

--- StudyHelper/src/main/java/by/ansgar/helper/controller/GroupController.java
+++ StudyHelper/src/main/java/by/ansgar/helper/controller/GroupController.java
@@ -1,6 +1,7 @@
 package by.ansgar.helper.controller;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -11,8 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import by.ansgar.helper.entity.Lessons;
+import by.ansgar.helper.entity.LinkStudentsLessonsRating;
 import by.ansgar.helper.entity.Students;
 import by.ansgar.helper.service.LessonsService;
+import by.ansgar.helper.service.LinkStudentsLessonsRatingsService;
 import by.ansgar.helper.service.StudentService;
 
 @Controller
@@ -24,14 +27,22 @@ public class GroupController {
 	private StudentService studentService;
 	@Autowired
 	private LessonsService lessonsService;
+	@Autowired
+	private LinkStudentsLessonsRatingsService lslrService;
 	
 	@RequestMapping(value="/view_group_{group}_lesson_{lesson}")
-	public ModelAndView showGroup(@PathVariable String group, @PathVariable String lesson){
+	public ModelAndView showGroup(@PathVariable String group, @PathVariable long lesson){
 		ModelAndView mav = new ModelAndView();
 		try {
 			List<Students> studentsByGroup = studentService.getStudentsByGroup(group);
 			List<Lessons> lessons = lessonsService.getAllLessons();
+			List<LinkStudentsLessonsRating> studRatingById = new ArrayList<LinkStudentsLessonsRating>();
+			for(int i = 0; i < studentsByGroup.size(); i++){
+				System.out.println(studentsByGroup.get(i).getId());
+				studRatingById.addAll(lslrService.getRatingsByStudAndLesson(lesson, studentsByGroup.get(i).getId()));
+			}
 			System.out.println(lesson);
+			mav.addObject("studRatings", studRatingById);
 			mav.addObject("lessons", lessons);
 			mav.addObject("group", group);
 			mav.addObject("studentsByGroup", studentsByGroup);
