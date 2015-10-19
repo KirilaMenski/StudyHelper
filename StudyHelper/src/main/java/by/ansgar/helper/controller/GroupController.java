e284f0f4a06660509a3d1c124129bc532617d412
Mensky <bv_1514@mail.ru>
2015-10-19 22:22:09 +0300

    In working

--- StudyHelper/src/main/java/by/ansgar/helper/controller/GroupController.java
+++ StudyHelper/src/main/java/by/ansgar/helper/controller/GroupController.java
@@ -7,12 +7,14 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import by.ansgar.helper.entity.Lessons;
 import by.ansgar.helper.entity.LinkStudentsLessonsRating;
+import by.ansgar.helper.entity.Ratings;
 import by.ansgar.helper.entity.Students;
 import by.ansgar.helper.service.LessonsService;
 import by.ansgar.helper.service.LinkStudentsLessonsRatingsService;
@@ -20,33 +22,52 @@ import by.ansgar.helper.service.StudentService;
 
 @Controller
 public class GroupController {
-	
+
 	private static final Logger LOG = Logger.getLogger(GroupController.class);
-	
+
 	@Autowired
 	private StudentService studentService;
 	@Autowired
 	private LessonsService lessonsService;
 	@Autowired
 	private LinkStudentsLessonsRatingsService lslrService;
-	
-	@RequestMapping(value="/view_group_{group}_lesson_{lesson}")
-	public ModelAndView showGroup(@PathVariable String group, @PathVariable long lesson){
+
+	@RequestMapping(value = "/view_group_{group}_lesson_{lesson}")
+	public ModelAndView showGroup(@PathVariable String group,
+			@PathVariable long lesson) {
 		ModelAndView mav = new ModelAndView();
 		try {
-			List<Students> studentsByGroup = studentService.getStudentsByGroup(group);
+			List<Students> studentsByGroup = studentService
+					.getStudentsByGroup(group);
 			List<Lessons> lessons = lessonsService.getAllLessons();
+			
 			List<LinkStudentsLessonsRating> studRatingById = new ArrayList<LinkStudentsLessonsRating>();
-			for(int i = 0; i < studentsByGroup.size(); i++){
-				System.out.println(studentsByGroup.get(i).getId());
-				studRatingById.addAll(lslrService.getRatingsByStudAndLesson(lesson, studentsByGroup.get(i).getId()));
-				System.out.println(studRatingById);
+			
+			for (int i = 0; i < studentsByGroup.size(); i++) {
+				studRatingById.addAll(lslrService.getRatingsByStudAndLesson(
+						lesson, studentsByGroup.get(i).getId()));
 			}
-			System.out.println(lesson);
+			
 			List<Integer> numb = new ArrayList<Integer>();
 			for(int i = 0; i < 10; i++){
 				numb.add(i);
 			}
+			
+			int [][] x = new int [5][5];
+			for(int i = 0; i < 5; i++){
+				
+				for(int j = 0; j < 5; j++){
+					x[i][j] = i;
+					System.out.print(x[i][j]);
+					int y = x[0][1];
+					mav.addObject("xj", x[j]);
+				}
+				System.out.println();
+				mav.addObject("xi", x[i]);
+			}
+			System.out.println(x[3][1]);
+			
+			
 			mav.addObject("numb", numb);
 			mav.addObject("studRatings", studRatingById);
 			mav.addObject("lessons", lessons);
@@ -56,9 +77,15 @@ public class GroupController {
 			e.printStackTrace();
 			LOG.warn(e);
 		}
-		
+
 		mav.setViewName("group");
 		return mav;
 	}
 
+	
+	@ModelAttribute("ratings")
+	public Ratings ratings() {
+		return new Ratings();
+	}
+	
 }
